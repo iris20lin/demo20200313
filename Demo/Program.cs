d88e8a6160d9009dfb0106586a16f2c514b874dc
wupojung <wupojung@gmail.com>
2020-03-12 20:17:04 +0800

    加入專案檔案。

---
+++ Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int hp = 100;
+
+            while (true)
+            {
+                //判斷死亡
+                if (hp <= 0)
+                {
+                    Console.WriteLine("你已經死了...");
+                    break;
+                }
+
+                //檢查是否有人攻擊
+                string attackString = Console.ReadLine();
+                int attack = int.Parse(attackString);
+
+                //扣血
+                hp = hp - attack;
+            }
+            Console.ReadLine();
+        }
+    }
+}
